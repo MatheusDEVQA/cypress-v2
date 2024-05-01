fafcbfc9a66ca84819795ce6b18940dc8989c33b
MatheusDEVQA <matheus_muniz3@hotmail.com.br>
2024-04-30 22:32:44 -0300

    Add new test to complete form

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,8 +1,31 @@
 /// <reference types="Cypress" />
 
-describe('Central de Atendimento ao Cliente TAT', function() {
-    it('verifica o título da aplicação', function() {
-  
+describe('Central de Atendimento ao Cliente TAT', function () {
+
+    beforeEach(function () {
+        cy.visit('./src/index.html')
     })
-  })
-  
+
+    it('verifica o título da aplicação', function () {
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    })
+
+    it('preenche os campos obrigatórios e envia o formulário', function () {
+        cy.get('#firstName').type('Matheus')
+        cy.get('#lastName').type('quality assurance')
+        cy.get('#email').type('math@email.com')
+        cy.get('#open-text-area').type('ajudar com algo teste de texto logo', {delay: 0 })
+        cy.get('button[type="submit"').click()
+        cy.get('.success').should('be.visible')
+    })
+
+    it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function(){
+        cy.get('#firstName').type('Matheus')
+        cy.get('#lastName').type('quality assurance')
+        cy.get('#email').type('math@email,com')
+        cy.get('#open-text-area').type('ajudar com algo teste de texto logo', {delay: 0 })
+        cy.get('button[type="submit"').click()
+
+        cy.get('.error').should('be.visible')
+    })
+})
